bcb68ebee7f6243fbcc00f493abf16ec573c91a1
lupekka <lucapezza1@gmail.com>
2026-03-07 06:30:13 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css'; // Carichiamo lo stile grafico migliorato
 
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
 
+// Abbiamo rimosso StrictMode per evitare doppi caricamenti e bug dei contatori
 root.render(
     <App />
 );
