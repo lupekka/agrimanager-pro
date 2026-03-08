cf131f8e5cf9be5e8b4430e4f0845a8d46ba2a1c
lupekka <lucapezza1@gmail.com>
2026-03-08 18:06:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import {
   PawPrint, CalendarDays, TrendingUp, Network, Baby, Trash2,
@@ -30,12 +31,12 @@ declare global {
 }
 
 const firebaseConfig = {
-** apiKey: process.env.VITE_FIREBASE_API_KEY,**
-** authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,**
-** projectId: process.env.VITE_FIREBASE_PROJECT_ID,**
-** storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,**
-** messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,**
-** appId: process.env.VITE_FIREBASE_APP_ID**
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
